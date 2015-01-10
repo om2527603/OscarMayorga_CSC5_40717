73412f56c7d5d1e98ce36bc3c70f064ac850d476
oscjos <oj.oscar.josue@gmail.com>
2015-01-10 01:15:32 -0800

    Signed-off-by: oscjos <oj.oscar.josue@gmail.com>

---
+++ Hmwr/Savltch_9thEd_Chap1_PracProg_Prob2/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: Oscar Mayorga 
+ * Created on January 9, 2015, 10:10 PM
+ * Porpuse: hmwr, Solution for a monitary value program.
+ */
+
+//System librery
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//User Library
+
+//Global Constants 
+
+//Prototype
+
+//Execution Begins here!
+int main( )
+{
+    int number_of_pods, peas_per_pod, total_peas;
+    cout<< "Hello! "<<endl;
+    cout<< "Press return after entering a number.";
+    cout<< "Enter the number of pods;|n";
+    cin>> number_of_pods;
+    cout<< "Enter the number of peas in a pod:|n";
+    cin>>peas_per_pod;
+    total_peas=number_of_pods*peas_per_pod;
+    cout<< "If you have ";
+    cout<< number_of_pods;
+    cout<< "Pods ";
+    cout<< "and ";
+    cout<< peas_per_pod;
+    cout<< "peas in each pod, then ";
+    cout<< "you have ";
+    cout<< total_peas;
+    cout<< " peas in all the pods.";
+    cout<< " Good-bye!";
+    return 0;
+}
