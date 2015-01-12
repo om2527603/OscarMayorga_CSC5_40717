7b416e9282441b183f03075fe3c933858894fc9f
oscjos <oj.oscar.josue@gmail.com>
2015-01-11 21:23:25 -0800

    Signed-off-by: oscjos <oj.oscar.josue@gmail.com>

---
+++ Hmwr/Assignment 1/Savltch_9thEd_Chap1_ProjProg_Prob2/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Oscar Mayorga 
+ * Created on January 11, 2015, 8:47 PM
+ * Purpose: Write a Program that Prints C S ! 
+ */
+
+//System Library 
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+////User Library
+
+//Global Constants 
+
+//Prototype
+
+//Execution Begins here!
+
+int main(int argc, char** argv) {
+cout<< "***********************************************************"<<endl;
+cout<<        "      C C C          S S S S          !!"<<endl;
+cout<<        "    C       C       S        S        !!"<<endl;
+cout<<        "   C               S                  !!"<<endl;
+cout<<        "  C                 S                 !!"<<endl;
+cout<<        "  C                  S S S            !!"<<endl;
+cout<<        "  C                         S         !!"<<endl;
+cout<<        "   C                         S        !!"<<endl;
+cout<<        "    C       C       S        S          "<<endl;
+cout<<        "      C C C           S S S S         00"<<endl;
+cout<< "***********************************************************"<<endl;
+cout<< "   Computer Science is Cool Stuff!!!"<<endl;
+
+    return 0;
+}
+
