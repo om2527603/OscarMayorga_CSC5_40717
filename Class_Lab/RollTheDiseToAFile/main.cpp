be03853c2dc6f12fdbea1c3deec88dc2883351f1
om2527603 <omayorga1@student.rcc.edu>
2015-01-12 10:52:08 -0800

    Signed-off-by: om2527603 <omayorga1@student.rcc.edu>

---
+++ Class_Lab/RollTheDiseToAFile/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on January 12, 2015, 10:07 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
